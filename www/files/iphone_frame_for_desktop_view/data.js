1a52d47ae51f061aa6c55f7c246636066dbfaad0
David Richard <drichard@designforuse.net>
2013-10-29 10:40:57 -0500

    First commit

---
+++ www/files/iphone_frame_for_desktop_view/data.js
@@ -0,0 +1,129 @@
+﻿$axure.loadCurrentPage({
+  "url":"iphone_frame_for_desktop_view.html",
+  "generationDate":new Date(1382990355178.27),
+  "isCanvasEnabled":false,
+  "variables":["OnLoadVariable"],
+  "page":{
+    "packageId":"b0f7e351c6884cbe8bb6b81b21b40992",
+    "type":"Axure:Page",
+    "name":"iPhone Frame for Desktop View",
+    "notes":{
+},
+    "style":{
+      "baseStyle":"627587b6038d43cca051c114ac41ad32",
+      "pageAlignment":"near",
+      "fill":{
+        "fillType":"solid",
+        "color":0xFFFFFFFF},
+      "image":null,
+      "imageHorizontalAlignment":"near",
+      "imageVerticalAlignment":"near",
+      "imageRepeat":"",
+      "favicon":null,
+      "sketchFactor":"0",
+      "colorStyle":"appliedColor",
+      "fontName":"Applied Font",
+      "borderWidth":"0"},
+    "adaptiveStyles":{
+},
+    "interactionMap":{
+},
+    "diagram":{
+      "objects":[{
+          "id":"0aa09586fcc6405eacf35f6f931f93fb",
+          "label":"iPhone Body",
+          "type":"imageBox",
+          "styleType":"imageBox",
+          "visible":true,
+          "style":{
+            "location":{
+              "x":20,
+              "y":30},
+            "size":{
+              "width":450,
+              "height":731},
+            "borderWidth":"-1"},
+          "adaptiveStyles":{
+},
+          "objects":[{
+              "id":"b1a768d151e14a08930532c3740d5090",
+              "label":"",
+              "isContained":true,
+              "type":"richTextPanel",
+              "styleType":"paragraph",
+              "visible":true,
+              "style":{
+                "location":{
+                  "x":20,
+                  "y":30},
+                "size":{
+                  "width":450,
+                  "height":731},
+                "borderWidth":"-1"},
+              "adaptiveStyles":{
+}}],
+          "images":{
+            "normal~":"images/iphone_frame_for_desktop_view/iphone_body_u0.png"}},
+{
+          "id":"0906c72064444fdaad91369770b347d6",
+          "label":"Inline Frame",
+          "type":"inlineFrame",
+          "styleType":"inlineFrame",
+          "visible":true,
+          "style":{
+            "location":{
+              "x":85,
+              "y":170},
+            "size":{
+              "width":320,
+              "height":460}},
+          "adaptiveStyles":{
+}},
+{
+          "id":"86707e36cdeb41ccba98822ebb6028fb",
+          "label":"Status Bar",
+          "type":"imageBox",
+          "styleType":"imageBox",
+          "visible":true,
+          "style":{
+            "location":{
+              "x":85,
+              "y":150},
+            "size":{
+              "width":320,
+              "height":20},
+            "borderWidth":"-1"},
+          "adaptiveStyles":{
+},
+          "objects":[{
+              "id":"342d15d2185b4faea76699f3a84598f0",
+              "label":"",
+              "isContained":true,
+              "type":"richTextPanel",
+              "styleType":"paragraph",
+              "visible":true,
+              "style":{
+                "location":{
+                  "x":85,
+                  "y":150},
+                "size":{
+                  "width":320,
+                  "height":20},
+                "borderWidth":"-1"},
+              "adaptiveStyles":{
+}}],
+          "images":{
+            "normal~":"images/iphone_frame_for_desktop_view/status_bar_u3.png"}}]}},
+  "masters":{
+},
+  "objectPaths":{
+    "0aa09586fcc6405eacf35f6f931f93fb":{
+      "scriptId":"u0"},
+    "b1a768d151e14a08930532c3740d5090":{
+      "scriptId":"u1"},
+    "0906c72064444fdaad91369770b347d6":{
+      "scriptId":"u2"},
+    "86707e36cdeb41ccba98822ebb6028fb":{
+      "scriptId":"u3"},
+    "342d15d2185b4faea76699f3a84598f0":{
+      "scriptId":"u4"}}});
